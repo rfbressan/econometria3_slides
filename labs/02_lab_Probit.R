e64367043a826bc446666e4e6ebc0601169bcf78
Rafael Bressan <rfbressan@gmail.com>
2022-09-16 10:49:03 -0300

    feat(labs): lab 02

--- labs/02_lab_Probit.R
+++ labs/02_lab_Probit.R
@@ -7,6 +7,7 @@
 library(wooldridge)
 library(fixest)
 library(modelsummary)
+library(ggplot2)
 #' ## Modelo de Probabilidade Linear
 #' 
 #' Se uma variável _dummy_ é usada como variável dependente, nós podemos usar
@@ -20,7 +21,8 @@ library(modelsummary)
 #' MPL sofrem de heterocedasticidade e, portanto, devemos utilizar erros padrão
 #' robustos.
 data("mroz")
-mpl <- feols(inlf~nwifeinc+educ+exper+I(exper^2)+age+kidslt6+kidsge6,
+fml <- inlf~nwifeinc+educ+exper+I(exper^2)+age+kidslt6+kidsge6
+mpl <- feols(fml,
              data = mroz,
              vcov = "HC1")
 msummary(mpl)
@@ -46,3 +48,115 @@ xpred <- data.frame(nwifeinc = c(100, 0),
 predict(mpl, newdata = xpred)
 #'
 #' ## Modelos Logit e Probit
+#' 
+#' Estes são modelos especializados em variáveis dependentes binárias e,
+#' portanto, não sofrem do problema de predição de probabilidade fora do 
+#' intervalo $\left[0,1\right]$. A um indexador linear, estes modelos aplicam
+#' uma função (não-linear) que garante $y\in \left[0,1\right]$. Esta é chamada 
+#' de função _link_.
+#' 
+#' * no modelo **probit** a função link é $G(z)=\Phi(z)$, a função de 
+#' distribuição da Normal, e;
+#' 
+#' * no modelo **logit** a função link é $G(z)=\Lambda(z)=\frac{\exp(z)}{1+\exp(z)}$,
+#' a função de distribuição Logística.
+#' 
+#' Desta forma, a probabilidade do evento é definida como:
+#' 
+#' $$P(y=1|x)=G(\beta_0 + \beta_1 x)$$
+#' 
+#' No `R` estimamos esses modelos através da função `glm`, de _Generalized
+#' Linear Models_, especificando o argumento `family`. Vejamos um exemplo.
+#' 
+logit <- glm(fml,
+             data = mroz,
+             family = binomial(link = logit))
+
+probit <- glm(fml,
+              data = mroz,
+              family = binomial(link = probit))
+
+msummary(list(Logit = logit, Probit = probit))
+#'
+#' Podemos fazer a de probabilidade das mesmas mulheres anteriores estarem no
+#' mercado de trabalho.
+#' 
+predict(logit, newdata = xpred, type = "response")
+predict(probit, newdata = xpred, type = "response")
+#'
+#' As previsões de logit e probit são semelhantes, mas especialmente nas caudas
+#' elas podem diferirem de forma significativa. O comando `predict` também
+#' pode retornar o valor do índice linear ($\hat\beta_0 + \hat\beta_1 x_i$) 
+#' quando o argumento for type = "link"
+#' 
+predict(logit, newdata = xpred, type = "link")
+predict(probit, newdata = xpred, type = "link")
+#'
+#' **Simule valores para o índice linear entre -5 e 5 e plote qual seriam as 
+#' respostas em probabilidades dos modelos logit e probit.**
+#+ echo=FALSE
+index <- seq(-5, 5, by = 0.5)
+logit_response <- plogis(index)
+probit_response <- pnorm(index)
+df <- data.frame(index, logit_response, probit_response)
+
+ggplot(df, aes(x = index)) +
+    geom_line(aes(y = logit_response, color = "Logit") ) +
+    geom_line(aes(y = probit_response, color = "Probit")) +
+    guides(color = guide_legend(title = "Modelo")) +
+    labs(x = "Índice linear",
+         y = "Probabilidade",
+         title = "Resposta em Probabilidade") +
+    theme_classic()
+#'
+#' ## Efeitos Parciais
+#' 
+#' Os parâmetros de modelos não-lineares como logit e probit não possuem uma
+#' interpretação direta dos efeitos _ceteris paribus_ do regressor sobre a 
+#' variável dependente. Uma medida utilizada é o efeito parcial (ou efeito
+#' marginal) que é dependente do ponto $x_i$ em que se faz a análise. Este
+#' efeito marginal é equivalente a inclinação da curva de resposta do modelo
+#' em um determinado ponto.
+#' 
+#' \begin{align}
+#' \frac{\partial\hat y}{\partial x}&=\frac{\partial G(x\hat\beta)}{\partial x}\\
+#' &=\hat\beta \cdot g(x\hat\beta)
+#' \end{align}
+#' 
+#' Onde é possível verificar que o efeito marginal depende tanto do valor
+#' estimado $\hat\beta$ quanto do ponto onde se faz a análise, $x$. O fato 
+#' de este efeito diferir por valor do regressor faz com que seja difícil 
+#' apresentar os resultados de uma maneira concisa. Adota-se, em geral, duas
+#' medidas de efeito parcial:
+#' 
+#' * Efeito Parcial na Média (PEA): $PEA(X_j)=g(\bar{x} \beta) \beta_j$
+#' 
+#' * Efeito Parcial Médio (APE): $APE(X_j)=\frac{1}{n} \sum_{i=1}^N g(x_i \beta) \beta_j$
+#' 
+#' **Calcule manualmente o PEA do modelo logit**
+#+ echo=FALSE
+# Ponto médio dos regressores
+regressors <- c("nwifeinc", "educ", "exper", "age", "kidslt6", "kidsge6")
+x_mean <- mroz[, regressors] |> 
+    colMeans() 
+betas <- coef(logit)
+# x_mean deve conter 1 na primeira posicao e exper^2 na quinta
+x_mean <- c(1, x_mean[1:3], mean(mroz[, "exper"]^2), x_mean[4:6])
+# xb
+index_mean <- sum(x_mean * betas)
+# Agora o PEA
+print("PEA de cada regressor é:")
+dlogis(index_mean) * betas
+
+# usando o pacote mfx
+mfx::logitmfx(fml, data = mroz, atmean = TRUE)
+#'
+#' **Calcule manualmente o APE do modelo logit**
+#+ echo=FALSE
+g_mean <- predict(logit) |> 
+    dlogis() |> 
+    mean()
+print("APE de cada regressor é:")
+betas * g_mean
+# usando o pacote mfx
+mfx::logitmfx(fml, data = mroz, atmean = FALSE)
